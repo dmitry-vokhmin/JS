414c6f0d1f13592a2d7b6c86dd34ec3713d8d467
Dmitry <boxdima1@gmail.com>
2021-05-18 10:12:22 -0400

    lesson_1

---
+++ lesson_1/main.js
@@ -0,0 +1,13 @@
+'use strict'
+
+var tc = 35;
+var tf = (9 / 5) * tc + 32;
+
+console.log(tf)
+
+var name = "Vasiliy";
+var admin = name;
+
+console.log(admin)
+
+//*Чему будет равно JS-выражение 1000 + "108" = 1000108
